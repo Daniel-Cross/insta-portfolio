61c709d0c4be57451d3196dbd7b55b8a42534438
Daniel-Cross <dan.s.cross@icloud.com>
2023-08-04 23:33:57 +0200

    added tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import "./styles/App.css";
 import Header from "./components/Header";
 import PhoneFrame from "./components/PhoneFrame";
+import ReactGA from "react-ga";
 
 function App() {
+  const TRACKING_ID = "G-85G1DWNL2G"; // YOUR_OWN_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <div className="App">
       <Header />
